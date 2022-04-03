86c02b6e234dd0076f92fd7f5c2e6e0ca02ec6ac
Forrest Wilbur <perfect.spellig@gmail.com>
2022-04-03 02:42:59 -0500

    Beginning efforts on getting mongodb working with the project.

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+let user = new Schema({
+    name: [
+        {
+            first: { 
+                type: String, 
+                required: [true, 'A person must have a first name.']
+            },
+            last: {
+                type: String, 
+                required: [true, 'A person must have a last name.']
+            }
+        }
+    ],
+    dob: {
+        type: Date,
+        required: [true, 'A person is born at a time, not everywhen or nowhen.'],
+        default: Date.now
+    },
+    registered: {
+        type: Date,
+        required: [true, 'A person joins democracy at a time, not everywhen or nowhen.'],
+        default: Date.now
+    },
+    lastUpdated: {
+        type: Date,
+        required: [true, 'A person always has somewhen that their file was updated, even if it\'s on file creation.'],
+        default: Date.now
+    },
+    team: {
+        type: Number,
+        required: [true, 'A person belongs to a team.']
+    }
+}, { collection: 'Users' })
+
+
+
+const User = mongoose.model('users', user)
+
+module.exports = User
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import TeamPointDisplay from '../components/team-points-display'
 import VotesChart from '../components/votes-chart'
 import VotesInfo from '../components/votes-info'
 import Voting from '../components/voting'
+// import dbConnect from '../lib/dbConnect'
+// import Vote from '../models/Vote'
+import clientPromise from '../lib/mongodb'
 
-export default function Home() {
+const Index = ({ isConnected }) => {
   return (
     <div>
       <Head>
@@ -47,3 +50,56 @@ export default function Home() {
     </div>
   )
 }
+/**
+export async function getServerSideProps() {
+  await dbConnect()
+
+  const rockResult = Vote.countDocuments({shape: 1, function(err, count) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("Count of rock votes: ", count)
+    }
+  }})
+
+  const paperResult = Vote.countDocuments({shape: 2, function(err, count) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("Count of paper votes: ", count)
+    }
+  }})
+
+  const scissorsResult = Vote.countDocuments({shape: 3})
+  
+  return { props: { rockVotes: JSON.parse(JSON.stringify(rockVotes)),
+                    paperVotes: JSON.parse(JSON.stringify(paperVotes)),
+                    scissorsVotes: JSON.parse(JSON.stringify(scissorsVotes)) }}
+}
+**/
+
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the folloing code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+
+export default Index
